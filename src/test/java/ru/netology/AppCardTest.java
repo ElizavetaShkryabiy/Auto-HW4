143d7abc647befd0b6bc85cee9027a2e6ca0a137
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-21 16:40:17 +0400

    All tests OK

--- src/test/java/ru/netology/AppCardTest.java
+++ src/test/java/ru/netology/AppCardTest.java
@@ -2,31 +2,35 @@ package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Objects;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardTest {
-    LocalDate date = LocalDate.now();
-    LocalDate returnvalue = date.plusDays(4);
+    String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd/MM/YYYY"));
+
 
     @Test
     public void shouldTestHappyPath() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Майкоп");
-        form.$("[data-test-id=date] [value]").clear();
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").find(byText("Забронировать")).click();
         $(".notification_visible").shouldBe(appear, Duration.ofSeconds(16));
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на 24.10.2021"));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на 25.10.2021"));
 
     }
 
@@ -36,7 +40,8 @@ public class AppCardTest {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Мытищи");
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Василий Пупкин");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
@@ -45,12 +50,14 @@ public class AppCardTest {
                 .shouldHave(exactText("Доставка в выбранный город недоступна"));
 
     }
+
     @Test
     public void shouldReturnErrorWhenCityNotInKirilitsa() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Moscow");
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Василий Пупкин");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
@@ -65,7 +72,8 @@ public class AppCardTest {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Майкоп");
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Vasya");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
@@ -74,12 +82,14 @@ public class AppCardTest {
                 .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
     }
+
     @Test
     public void shouldReturnErrorWhenWrongFormatOfTel() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Майкоп");
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Вася");
         form.$("[data-test-id=phone] input").setValue("89271112233");
         form.$("[data-test-id=agreement]").click();
@@ -88,12 +98,14 @@ public class AppCardTest {
                 .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
+
     @Test
     public void shouldReturnErrorWhenAgreementEmpty() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Майкоп");
-        form.$("[data-test-id=date] input").setValue(String.valueOf(returnvalue));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(String.valueOf(date));
         form.$("[data-test-id=name] input").setValue("Вася");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]");
@@ -106,12 +118,13 @@ public class AppCardTest {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Майкоп");
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         form.$("[data-test-id=date] input").setValue("80.56.8532");
         form.$("[data-test-id=name] input").setValue("Вася");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").find(byText("Забронировать")).click();
-        $(".input_invalid[data-test-id=date] .input__sub").shouldBe(appear)
+        $("[data-test-id=date] .input_invalid .input__sub").shouldBe(appear)
                 .shouldHave(exactText("Неверно введена дата"));
 
     }
