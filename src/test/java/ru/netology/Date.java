5bfcbfc4d1570ff9b828bdf69afb94ac94355ca8
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-24 18:37:23 +0400

    tests for calendar + methods for date added

--- src/test/java/ru/netology/Date.java
+++ src/test/java/ru/netology/Date.java
@@ -1,7 +1,14 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class Date {
     public static String getDate(int amount) {
@@ -9,6 +16,49 @@ public class Date {
         return date;
     }
 
+    public static LocalDate getDatePlusWeeks(int amount) {
+        LocalDate date = LocalDate.now().plusWeeks(amount);
+        return date;
+    }
+
+    public static String getFullDateInCalendar(int amount) {
+        String date = getDatePlusWeeks(amount).format(DateTimeFormatter.ofPattern("dd.MM.YYYY"));
+        return date;
+    }
+
+    public static String getDateForCalendar(int amount) {
+        String date = String.valueOf(getDatePlusWeeks(amount).getDayOfMonth());
+        return date;
+    }
 
 
+    public static String getMonthPlusWeeks(int amount) {
+        String date = String.valueOf(LocalDate.now().plusWeeks(amount).getMonth());
+        return date;
+    }
+
+    public static String getMonth() {
+        String date = String.valueOf(LocalDate.now().getMonth());
+        return date;
+    }
+
+    public static void findDate() {
+        $$(".calendar__day")
+                .find(Condition.text(Date.getDateForCalendar(2)))
+                .click();
+    }
+
+    public static void findPage() {
+        SelenideElement form = $(".form");
+        String dateOfMeeting = Date.getMonthPlusWeeks(2);
+        String today = Date.getMonth();
+        if (today != dateOfMeeting) {
+            $$("[data-step]")
+                    .last()
+                    .click();
+            Date.findDate();
+        } else {
+            Date.findDate();
+        }
+    }
 }
--- src/test/java/ru/netology/ElementsTest.java
+++ src/test/java/ru/netology/ElementsTest.java
@@ -1,17 +1,14 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -44,15 +41,14 @@ public class ElementsTest {
         form.$("[data-test-id=city] input").setValue("Майкоп");
         form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         form.$(".input__icon").click();
-        $$(".calendar__day")
-                .find(Condition.text(String.valueOf(LocalDate.now().plusWeeks(1).getDayOfMonth())))
-                .click();
-        form.$("[data-test-id=name] input").setValue("Вася");
+        Date.findPage();
+        form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").find(byText("Забронировать")).click();
         $(".notification_visible").shouldBe(appear, Duration.ofSeconds(16));
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.YYYY"))));
+        $("[data-test-id=notification]")
+                .shouldHave(text("Встреча успешно забронирована на " + Date.getFullDateInCalendar(2)));
 
     }
 }
